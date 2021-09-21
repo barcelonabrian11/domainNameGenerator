1092ab805a1b7ddc2f77b41d102219b650945944
barcelonabrian11 <barcelonabrian11@gmail.com>
2021-09-21 10:28:31 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let extension = [".com", ".us", ".net",".io"]
+  for (let i = 0; i < pronoun.length; i++) {
+ 
+    for (let j = 0; j < adj.length; j++){
+   
+      for(let m = 0; m < noun.length; m++){
+        
+        for(let n = 0; n < extension.length; n++) {
+          console.log(pronoun[i]+adj[j]+noun[m]+extension[n]);
+        }    }
+    }
+  }
